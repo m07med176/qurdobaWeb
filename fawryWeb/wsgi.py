fcaf11c513be5d027f5b892476855bea09e21978
m07med176 <mohamed.arafa176@gmail.com>
2020-11-02 12:59:28 +0200

    Allowed host1111

--- fawryWeb/wsgi.py
+++ fawryWeb/wsgi.py
@@ -13,7 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fawryWeb.settings')
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
