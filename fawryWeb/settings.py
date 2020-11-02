bd0780031aaab22ed403cf5879a00e5e136a1477
m07med176 <mohamed.arafa176@gmail.com>
2020-11-02 10:10:52 +0200

    Procfile and req5

--- fawryWeb/settings.py
+++ fawryWeb/settings.py
@@ -124,10 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-DISABLE_COLLECTSTATIC=1
